b1fdb84adae5672b403b27db619c380219332555
algebrachan <1344990117@qq.com>
2020-11-27 17:01:41 +0800

    add 注释

--- src/models/example.js
+++ src/models/example.js
@@ -11,12 +11,14 @@ export default {
   },
 
   effects: {
+    // 异步方法
     *fetch({ payload }, { call, put }) {  // eslint-disable-line
       yield put({ type: 'save' });
     },
   },
 
   reducers: {
+    // 修改 state
     save(state, action) {
       return { ...state, ...action.payload };
     },
